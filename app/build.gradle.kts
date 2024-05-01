0175dfd6d74e25d20abe49350fce07526420b28d
FERNANDEZ Orlando <ofernandez-out@redoute.pt>
2024-05-01 23:53:30 +0100

    setup AndroidApplicationConventionPlugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,13 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.mapsPlatformSecretsPlugin)
+    alias(libs.plugins.runningtracker.android.application)
 }
 
 android {
+
     namespace = "io.github.orlandroyd.runningtracker"
-    compileSdk = 34
-
+    
     defaultConfig {
-        applicationId = "io.github.orlandroyd.runningtracker"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -26,13 +19,6 @@ android {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
--- build-logic/convention/src/main/java/io/github/orlandroyd/convention/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package io.github.orlandroyd.convention
-
-class MyClass {
-}
