4b04e69142c4ffea5a4bd612af0d333e34abb238
FERNANDEZ Orlando <ofernandez-out@redoute.pt>
2024-05-05 14:22:26 +0100

    setup AndroidFeatureUiConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -30,5 +30,9 @@ gradlePlugin {
             id = "runningtracker.android.library.compose"
             implementationClass = "AndroidLibraryComposeConventionPlugin"
         }
+        register("androidFeatureUi") {
+            id = "runningtracker.android.feature.ui"
+            implementationClass = "AndroidFeatureUiConventionPlugin"
+        }
     }
 }
