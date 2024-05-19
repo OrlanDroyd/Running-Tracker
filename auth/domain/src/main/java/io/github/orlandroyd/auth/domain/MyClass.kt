0c76dca630dd9d8aa3445b40325cc73f63e00314
FERNANDEZ Orlando <ofernandez-out@redoute.pt>
2024-05-19 23:25:53 +0100

    add RegisterScreen

--- auth/domain/src/main/java/io/github/orlandroyd/auth/domain/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package io.github.orlandroyd.auth.domain
-
-class MyClass {
-}
---
+++ auth/domain/src/main/java/io/github/orlandroyd/auth/domain/UserDataValidator.kt
@@ -0,0 +1,5 @@
+package io.github.orlandroyd.auth.domain
+
+object UserDataValidator {
+    const val MIN_PASSWORD_LENGTH = 9
+}
