b2a5c3eef1ff631538558f3d863edfccc91cf3c2
FERNANDEZ Orlando <ofernandez-out@redoute.pt>
2024-06-23 16:15:23 +0100

    setup run models

--- core/presentation/ui/src/main/java/io/github/orlandroyd/core/presentation/ui/RunDataFormatters.kt
+++ core/presentation/ui/src/main/java/io/github/orlandroyd/core/presentation/ui/RunDataFormatters.kt
@@ -19,16 +19,23 @@ fun Double.toFormattedKm(): String {
 }
 
 fun Duration.toFormattedPace(distanceKm: Double): String {
-
     if (this == Duration.ZERO || distanceKm <= 0.0) {
         return "-"
     }
 
-    val secondsPerKm = (inWholeSeconds / distanceKm).roundToInt()
+    val secondsPerKm = (this.inWholeSeconds / distanceKm).roundToInt()
     val avgPaceMinutes = secondsPerKm / 60
     val avgPaceSeconds = String.format("%02d", secondsPerKm % 60)
 
     return "$avgPaceMinutes:$avgPaceSeconds / km"
+}
+
+fun Double.toFormattedKmh(): String {
+    return "${roundToDecimals(1)} km/h"
+}
+
+fun Int.toFormattedMeters(): String {
+    return "$this m"
 }
 
 private fun Double.roundToDecimals(decimalCount: Int): Double {
