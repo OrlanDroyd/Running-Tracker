5e4ba51c54e93e44deb9cb1d927d15b287757dcc
FERNANDEZ Orlando <ofernandez-out@redoute.pt>
2024-07-14 15:43:45 +0100

    add sync strategy

--- core/database/src/main/java/io/github/orlandroyd/core/database/RunDatabase.kt
+++ core/database/src/main/java/io/github/orlandroyd/core/database/RunDatabase.kt
@@ -3,13 +3,21 @@ package io.github.orlandroyd.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import io.github.orlandroyd.core.database.dao.RunDao
+import io.github.orlandroyd.core.database.dao.RunPendingSyncDao
+import io.github.orlandroyd.core.database.entity.DeletedRunSyncEntity
 import io.github.orlandroyd.core.database.entity.RunEntity
+import io.github.orlandroyd.core.database.entity.RunPendingSyncEntity
 
 @Database(
-    entities = [RunEntity::class],
+    entities = [
+        RunEntity::class,
+        RunPendingSyncEntity::class,
+        DeletedRunSyncEntity::class
+    ],
     version = 1
 )
 abstract class RunDatabase : RoomDatabase() {
 
     abstract val runDao: RunDao
+    abstract val runPendingSyncDao: RunPendingSyncDao
 }
